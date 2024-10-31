4a122f170fa8e58661072a18f5e606db5ce32260
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-10-31 15:19:58 -0300

    add UserCredentials entity

---
+++ lib/features/authentication/domain/entities/user_credentials.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class UserCredentials extends Equatable {
+  final String userName;
+  final String password;
+
+  const UserCredentials({
+    required this.userName,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [userName, password];
+}
